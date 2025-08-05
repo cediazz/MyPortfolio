9eaef6f0e630d4218ad1cddb467b24ad2272307b
Carlos Ernesto Diaz Acosta <carlos.diaz@24hb101l-115366.corp.etecsa.cu>
2025-08-05 15:18:21 -0400

    modificado allowed_host

--- Backend/PortfolioAPI/PortfolioAPI/settings.py
+++ Backend/PortfolioAPI/PortfolioAPI/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '3441sd-g1@^7497shob#@sxwmt73at$k!%x#^(o=-6_4awpr$1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

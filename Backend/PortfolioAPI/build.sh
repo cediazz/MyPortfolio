7c60178db6143e8b625b2e01de2309807015cdf6
Carlos Ernesto Diaz Acosta <carlos.diaz@24hb101l-115366.corp.etecsa.cu>
2025-08-05 11:44:07 -0400

    Agregado cambio de ruta al build.sh

--- Backend/PortfolioAPI/build.sh
+++ Backend/PortfolioAPI/build.sh
@@ -1,8 +1,8 @@
-
+#!/bin/bash
 set -o errexit
 
+cd Backend/PortfolioAPI
+
 uv pip install -r requirements.txt
-
 uv run manage.py collectstatic --no-input
-
-uv run manage.py migrate
+#uv run manage.py migrate

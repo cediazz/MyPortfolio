d3709cec5777d0c6ee0f34aa057407b43251733a
Carlos Ernesto Diaz Acosta <carlos.diaz@24hb101l-115366.corp.etecsa.cu>
2025-08-05 16:03:32 -0400

    Agregada doc swagger y arreglado rutas

--- Backend/PortfolioAPI/PortfolioAPI/urls.py
+++ Backend/PortfolioAPI/PortfolioAPI/urls.py
@@ -19,11 +19,30 @@ from django.urls import path,include
 from .routers import router
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Portfolio API",
+      default_version='v1',
+      description="Portfolio",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('projects/', include('project_management.urls')),
-    path('cvs/', include('curriculum_management.urls')),
+    path('api/doc<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/projects/', include('project_management.urls')),
+    path('api/cvs/', include('curriculum_management.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- Backend/PortfolioAPI/curriculum_management/views.py
+++ Backend/PortfolioAPI/curriculum_management/views.py
@@ -2,11 +2,22 @@ from .models import Curriculum
 from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from .models import Curriculum
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import api_view
 
-
+@swagger_auto_schema(
+        method='GET',
+        operation_description="Descargar CV",
+        tags=["Curriculum"],
+        responses={
+            200: 'CV en PDF',
+            400: 'Error'
+        },
+        
+)
+@api_view(['GET'])
 def download_cv(request):
     cv = get_object_or_404(Curriculum)
-    print(cv)
     file = open(cv.file.path, 'rb')
     response = FileResponse(file)
     response['Content-Disposition'] = f'attachment; filename="{cv.file.name}"'

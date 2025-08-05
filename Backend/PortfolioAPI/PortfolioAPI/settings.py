965c302feadae24cd54b86ab8e4eea9bf0381a7e
Carlos Ernesto Diaz Acosta <carlos.diaz@24hb101l-115366.corp.etecsa.cu>
2025-08-05 11:13:50 -0400

    arreglado build.sh

--- Backend/PortfolioAPI/PortfolioAPI/settings.py
+++ Backend/PortfolioAPI/PortfolioAPI/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+print(DATABASES['default']['NAME'])
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
